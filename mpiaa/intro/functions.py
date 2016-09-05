0e6e3bfcdcdfc24b249eae44c2c76d8ab4d6d99e
Georgy Schukin <georgy.schukin@gmail.com>
2016-09-06 00:41:05 +0700

    Add new functions in intro example

--- mpiaa/intro/functions.py
+++ mpiaa/intro/functions.py
@@ -2,9 +2,30 @@ from mpiaa.timer import time_me, powers_of
 
 
 def find(items, item_to_find):
-    """Returns True if given item is in given list of items.
-    Returns False otherwise"""
+    """Returns True if given list of items contains given item.
+    Returns False otherwise."""
     return False
+
+
+def find_if(items, pred):
+    """Returns first item for which given predicate is satisfied.
+    Returns None if there is no such item."""
+    return None
+
+
+def find_all_if(items, pred):
+    """Returns all items for which given predicate is satisfied.
+    Returns empty list if there are no such items."""
+    return []
+
+
+def count_if(items, pred):
+    """Returns number of items for which given predicate is satisfied."""
+
+
+def all_unique(items):
+    """Returns True if all items are unique.
+    Returns False otherwise."""
 
 
 if __name__ == "__main__":
