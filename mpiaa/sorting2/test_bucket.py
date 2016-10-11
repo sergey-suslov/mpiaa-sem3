9f7b213b9a56dfd3ef15ad8aa591fcf220fe908c
Georgy Schukin <georgy.schukin@gmail.com>
2016-10-11 13:05:56 +0700

    Add unit tests

--- mpiaa/sorting2/test_bucket.py
+++ mpiaa/sorting2/test_bucket.py
@@ -7,6 +7,14 @@ from mpiaa.sorting2.sortings2 import bucket_sort
 class BucketSortTests(SortTests):
     def setUp(self):
         self.set_sort_func(bucket_sort)
+        self.pair_key = lambda el: el[1]
+
+    def test_pairs_single(self):
+        self.assertEqual(bucket_sort([("A", 1)], self.pair_key), [("A", 1)])
+
+    def test_pairs_multiple(self):
+        self.assertEqual(bucket_sort([("A", 1), ("B", 0), ("C", -1)], self.pair_key),
+                         [("C", -1), ("B", 0), ("A", 1)])
 
 
 if __name__ == "__main__":
--- mpiaa/sorting2/test_counting.py
+++ mpiaa/sorting2/test_counting.py
@@ -7,6 +7,14 @@ from mpiaa.sorting2.sortings2 import counting_sort
 class CountSortTests(SortTests):
     def setUp(self):
         self.set_sort_func(counting_sort)
+        self.pair_key = lambda el: el[1]
+
+    def test_pairs_single(self):
+        self.assertEqual(counting_sort([("A", 1)], self.pair_key), [("A", 1)])
+
+    def test_pairs_multiple(self):
+        self.assertEqual(counting_sort([("A", 1), ("B", 0), ("C", -1)], self.pair_key),
+                         [("C", -1), ("B", 0), ("A", 1)])
 
 
 if __name__ == "__main__":
