c5678c1ca5a09a6e48f052e90445837d89e54d3f
Georgy Schukin <georgy.schukin@gmail.com>
2016-11-15 10:34:13 +0700

    Update graph class

--- mpiaa/graphs/graph.py
+++ mpiaa/graphs/graph.py
@@ -1,5 +1,5 @@
 class Graph(object):
-    """Graph clas"""
+    """Graph class"""
     def __init__(self, vertices=[], edges=[]):
         self.vertices = set()
         self.adjacent = {}
@@ -10,11 +10,13 @@ class Graph(object):
 
     def add_vertex(self, vertex):
         """Add vertex to the graph."""
+        self.vertices.add(vertex)
+        """Add vertex to the graph."""
         if vertex not in self.adjacent:
             self.adjacent[vertex] = []
 
     def add_edge(self, v1, v2):
-        """Add edge (bidirectional) to the graph."""
+        """Add edge to the graph between vertices v1 and v2."""
         self.add_vertex(v1)
         self.add_vertex(v2)
         v1_adj = self.adjacent[v1]
@@ -24,11 +26,21 @@ class Graph(object):
         if v1 not in v2_adj:
             v2_adj.append(v1)
 
+    def get_vertices(self):
+        """Get all vertices of the graph."""
+        return list(self.vertices)
+
     def get_adjacent(self, vertex):
         """Get list of adjacent vertices in the graph for the vertex"""
         if vertex in self.adjacent:
             return self.adjacent[vertex]
         return []
+
+    def has_edge(self, v1, v2):
+        """Returns True if there is an edge between vertices v1 and v2, False otherwise."""
+        if v1 not in self.vertices or v2 not in self.vertices:
+            return False
+        return v2 in self.adjacent[v1]
 
 
 
