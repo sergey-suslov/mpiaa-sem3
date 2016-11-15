0d55f040248a5e3e6dfbe874f039533ad256b15e
Georgy Schukin <georgy.schukin@gmail.com>
2016-11-15 13:13:07 +0700

    Add unit tests for graph

---
+++ mpiaa/graphs/test_graph.py
@@ -0,0 +1,30 @@
+from mpiaa.graphs.graph import Graph
+import unittest
+
+
+class GraphTests(unittest.TestCase):
+    def setUp(self):
+        self.graph = Graph()
+
+    def test_empty(self):
+        self.assertEqual(self.graph.get_vertices(), [])
+
+    def test_add_edge(self):
+        self.graph.add_edge("A", "B")
+        self.assertEqual(len(self.graph.get_vertices()), 2)
+        self.assertTrue(self.graph.has_edge("A", "B"))
+        self.assertTrue(self.graph.has_edge("B", "A"))
+        self.assertIn("B", self.graph.get_adjacent("A"))
+
+    def test_add_edges(self):
+        self.graph.add_edge("A", "B")
+        self.graph.add_edge("A", "C")
+        self.graph.add_vertex("D")
+        self.assertEqual(len(self.graph.get_vertices()), 4)
+        self.assertIn("C", self.graph.get_adjacent("A"))
+        self.assertNotIn("D", self.graph.get_adjacent("A"))
+        self.assertFalse(self.graph.has_edge("A", "D"))
+
+
+if __name__ == "__main__":
+    unittest.main()
