8fec1a83a881e6cfa9c34025e6ea21721ebd27ee
Sergey <dagerok22@gmail.com>
2016-12-03 23:10:03 +0700

    Everything works

--- mpiaa/graphs_lab/__init__.py
+++ mpiaa/graphs_lab/__init__.py
@@ -2,6 +2,8 @@ from random import random
 
 import time
 
+import sys
+
 from mpiaa.graphs_lab.graph import Graph
 from mpiaa.graphs_lab import StrongConnectivity, DFS
 
@@ -10,10 +12,10 @@ def practise():
     lab_graph = Graph()
     with open('src/graph.txt') as graph_file:
         graph_file = graph_file.read()
-        for adj in graph_file.split('\n')[:200000]:
+        for adj in graph_file.split('\n')[:15000]:
             pair = adj.split('\t')
             lab_graph.add_edge(str(pair[0]), str(pair[1]), int(100*random()))
-    graph_dict = {x: lab_graph.get_adjacent(x) for x in lab_graph.get_vertices()}
+    graph_dict = lab_graph.adjacent
     print("Graph is filled")
 
     #Custom Sedgwick#######################################
@@ -21,17 +23,32 @@ def practise():
     t1 = time.time()
     components = lab_graph.kosaraju()
     t2 = time.time()
-    len_of_comps = len(components)
-    print("Strongy connected components kosaraju " + str(t2 - t1) + " num of comps: " + str(len_of_comps))
+    num_of_comps = len(components)
+    print("Strongy connected components kosaraju " + str(t2 - t1) + " num of comps: " + str(num_of_comps))
     ######################################################
 
     #Lib Sedgwick##########################################
     t1 = time.time()
     components = StrongConnectivity.Condensation(graph_dict)
     t2 = time.time()
-    len_of_comps = len(components)
-    print("Strongy connected components lib's " + str(t2 - t1) + " num of comps: " + str(len_of_comps))
+    num_of_comps = len(components)
+    sum = 0
+    min_len = sys.maxsize
+    max_len = 0
+    for i in components:
+        current_len = len(i)
+        sum+= current_len
+        if  current_len > max_len:
+            max_len = current_len
+        if current_len < min_len:
+            min_len = current_len
+    average_len = sum / num_of_comps
+
+    print("Strongy connected components lib's " + str(t2 - t1) + " num of comps: " + str(num_of_comps))
+    print("min, max, average length of components: " + str(min_len) + ", " + str(max_len) + ", " + str(average_len))
     ######################################################
+
+
 
     #Ostov################################################
     print("Ostov " + str(lab_graph.get_ostov_minimal_weight()))
