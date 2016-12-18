f5f23a47ea32b8495cf1b61e38e7e228daaabdf3
Sergey <dagerok22@gmail.com>
2016-12-18 22:23:31 +0700

    Lab done

--- mpiaa/rgz/init.py
+++ mpiaa/rgz/init.py
@@ -1,3 +1,5 @@
+import heapq
+
 import networkx as nx
 import matplotlib.pyplot as plt
 import networkx as nx
@@ -20,24 +22,73 @@ class ClickCount:
     def increase(self):
         self.click_count += 1
 
+class PriorityQueue:
+    def __init__(self):
+        self.elements = []
+
+    def empty(self):
+        return len(self.elements) == 0
+
+    def put(self, item, priority):
+        heapq.heappush(self.elements, (priority, item))
+
+    def get(self):
+        return heapq.heappop(self.elements)[1]
+
+def heuristic(a, b):
+    (x1, y1) = a
+    (x2, y2) = b
+    return abs(x1 - x2) + abs(y1 - y2)
+
+def a_star_search(graph, start, goal):
+    frontier = PriorityQueue()
+    frontier.put(start, 0)
+    came_from = {}
+    cost_so_far = {}
+    came_from[start] = None
+    cost_so_far[start] = 0
+
+    while not frontier.empty():
+        current = frontier.get()
+
+        if current == goal:
+            break
+
+        for next in graph.neighbors(current):
+            new_cost = cost_so_far[current] + 1
+            if next not in cost_so_far or new_cost < cost_so_far[next]:
+                cost_so_far[next] = new_cost
+                priority = new_cost + heuristic(goal, next)
+                frontier.put(next, priority)
+                came_from[next] = current
+
+    path = [goal]+[came_from[goal]]
+    while path[-1] != start:
+        path.append(came_from[path[-1]])
+    return path[::-1]
+
+
+
 if __name__ == "__main__":
 
     G = nx.Graph()
-    M, N = 3, 7
+    M, N = 8, 10
     ### Nodes
     checkpoints, start_finish = [], []
+    path_edges = []
     allNodes = list(range(M*N))
 
     count = 0
 
 
-    def refreshGraph():
+    def refreshGraph(edges_to_colorize=[]):
         plt.clf()
         nx.draw_networkx_nodes(G, pos, nodelist=allNodes, node_color='k', node_size=100, alpha=0.8)
         nx.draw_networkx_nodes(G, pos, nodelist=checkpoints, node_color='w', node_size=100, alpha=0.8)
         nx.draw_networkx_nodes(G, pos, nodelist=start_finish[:1], node_color='g', node_size=100, alpha=0.8)
         nx.draw_networkx_nodes(G, pos, nodelist=start_finish[1:], node_color='r', node_size=100, alpha=0.8)
-        nx.draw_networkx_edges(G, pos, edge_color='k', width=1.0, alpha=0.5)
+        nx.draw_networkx_edges(G, pos, style='dotted', edge_color='k', width=1.0, alpha=0.5)
+        nx.draw_networkx_edges(G, pos, edgelist=edges_to_colorize, edge_color='b', width=3.0, alpha=0.7)
         plt.axis('on')
         plt.axis((-1, M + 1, -1, N + 1))
         fig.patch.set_facecolor('white')
@@ -63,13 +114,15 @@ if __name__ == "__main__":
         passed_point = start
         current_start = start
         while len(checkpoints) and flag:
+            min_len = pylab.sys.maxsize
+            min_path = []
             for i in checkpoints:
-                path = list(bfs_paths(graph, current_start, i))[0]
+                path = a_star_search(graph, current_start, i)
                 if len(path) < min_len:
                     min_path = path
                     min_len = len(path)
                     passed_point = i
-            result += min_path
+            result += min_path[1:]
             if len(checkpoints) == 1:
                 flag = False
             if current_start in checkpoints:
@@ -77,10 +130,18 @@ if __name__ == "__main__":
             current_start = passed_point
             if current_start in checkpoints:
                 checkpoints.remove(current_start)
-        path = list(bfs_paths(graph, current_start, finish))[0]
+        path = a_star_search(graph, current_start, finish)
         result += path[1:]
-        return result
+        return [start] + result
 
+    def update_edges(nodes=[]):
+        edges = []
+        current_node = nodes[0]
+        for i in nodes[1:]:
+            edges.append((current_node, i))
+            current_node = i
+        refreshGraph(edges_to_colorize=edges)
+        pass
 
     def onClick(event):
         (x, y) = (event.xdata, event.ydata)
@@ -89,8 +150,8 @@ if __name__ == "__main__":
                 graph = G.adj
                 g_key = graph[(1, 1)].keys()
                 graph = {i: set([j for j in graph[i].keys()]) for i in graph.keys()}
-                a = algorithm(graph, start_finish[0], start_finish[1], checkpoints)
-                print(a)
+                a = algorithm(G, start_finish[0], start_finish[1], checkpoints[:])
+                update_edges(a)
             # setUp()
         else:
             for i in allNodes:
